c2d513126027dfb10ac97d8ceef9dcfa9c14f89b
Clément Faure <clement.faure@etudiant.univ-reims.fr>
2020-11-26 11:57:45 +0100

    Ajout du code pour envoyer le SMS

--- app/src/main/java/com/lpro/badgeuse/MainActivity.java
+++ app/src/main/java/com/lpro/badgeuse/MainActivity.java
@@ -1,14 +1,136 @@
 package com.lpro.badgeuse;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int MY_PERMISSION_REQUEST_CODE_SEND_SMS = 1;
+
+    private static final String LOG_TAG = "AndroidExample";
+
+    private EditText phoneForm;
+    private EditText messageForm;
+    private Button sendButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.phoneForm = (EditText) this.findViewById(R.id.phone);
+        this.messageForm = (EditText) this.findViewById(R.id.message);
+
+        this.sendButton = (Button) this.findViewById(R.id.button);
+
+        this.sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                askPermissionAndSendSMS();
+            }
+        });
+    }
+
+    private void askPermissionAndSendSMS() {
+        if (android.os.Build.VERSION.SDK_INT >=  android.os.Build.VERSION_CODES.M) { // 23
+
+            // Vérifier permission d'envoyer des SMS
+            int sendSmsPermisson = ActivityCompat.checkSelfPermission(this,
+                    Manifest.permission.SEND_SMS);
+
+            if (sendSmsPermisson != PackageManager.PERMISSION_GRANTED) {
+                // If don't have permission so prompt the user.
+                this.requestPermissions(
+                        new String[]{Manifest.permission.SEND_SMS},
+                        MY_PERMISSION_REQUEST_CODE_SEND_SMS
+                );
+                return;
+            }
+        }
+        this.sendSMS_by_smsManager();
+    }
+    private void sendSMS_by_smsManager()  {
+
+        String phoneNumber = this.phoneForm.getText().toString();
+        String message = this.messageForm.getText().toString();
+
+        try {
+            // Obtenir l'instance du manager SMS
+            SmsManager smsManager = SmsManager.getDefault();
+            // Envoyer le SMS
+            smsManager.sendTextMessage(phoneNumber,
+                    null,
+                    message,
+                    null,
+                    null);
+
+            Log.i( LOG_TAG,"Your sms has successfully sent!");
+            Toast.makeText(getApplicationContext(),"Your sms has successfully sent!",
+                    Toast.LENGTH_LONG).show();
+        } catch (Exception ex) {
+            Log.e( LOG_TAG,"Your sms has failed...", ex);
+            Toast.makeText(getApplicationContext(),"Your sms has failed... " + ex.getMessage(),
+                    Toast.LENGTH_LONG).show();
+            ex.printStackTrace();
+        }
+    }
+
+
+    // When you have the request results
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        //
+        switch (requestCode) {
+            case MY_PERMISSION_REQUEST_CODE_SEND_SMS: {
+
+                // Note: If request is cancelled, the result arrays are empty.
+                // Permissions granted (SEND_SMS).
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    Log.i( LOG_TAG,"Permission granted!");
+                    Toast.makeText(this, "Permission granted!", Toast.LENGTH_LONG).show();
+
+                    this.sendSMS_by_smsManager();
+                }
+                // Cancelled or denied.
+                else {
+                    Log.i( LOG_TAG,"Permission denied!");
+                    Toast.makeText(this, "Permission denied!", Toast.LENGTH_LONG).show();
+                }
+                break;
+            }
+        }
+    }
+
+    // When results returned
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == MY_PERMISSION_REQUEST_CODE_SEND_SMS) {
+            if (resultCode == RESULT_OK) {
+                // Do something with data (Result returned).
+                Toast.makeText(this, "Action OK", Toast.LENGTH_LONG).show();
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "Action canceled", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Action Failed", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
