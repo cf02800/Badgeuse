0f946808524bebe8d7c983d1f500bfbbfd8d27b5
Romain <romain.duciel08@gmail.com>
2020-12-03 16:38:31 +0100

    Ajout parametrages au lancement de l'application

---
+++ app/src/main/java/com/lpro/badgeuse/GlobalActivity.java
@@ -0,0 +1,30 @@
+package com.lpro.badgeuse;
+
+import android.app.Application;
+import android.content.Intent;
+import android.content.SharedPreferences;
+
+import androidx.preference.PreferenceManager;
+
+public class GlobalActivity extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        SharedPreferences userInfo = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        SharedPreferences.Editor editor = userInfo.edit();
+        String Numero = userInfo.getString("phoneNo","");
+        String nom = userInfo.getString("nom", "");
+        if((nom.isEmpty()) || (Numero.isEmpty()))
+        {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+
+        }
+        else {
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+        }
+    }
+}
